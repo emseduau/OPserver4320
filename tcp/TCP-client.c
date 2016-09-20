f20a7396fecfef6df8ec90d8288160524821165f
sethdubs <sethdubs@gmail.com>
2016-09-20 14:49:01 -0500

    Removed unused functions and add a lot of comments

--- tcp/TCP-client.c
+++ tcp/TCP-client.c
@@ -27,17 +27,17 @@
 #define RSOP 4
 #define LSOP 5
 
-typedef struct int16Store{ unsigned char theBytes[3]; int16_t theInt; } int16Store;
-typedef struct int32Store{ unsigned char theBytes[5]; int32_t theInt; } int32Store;;
+typedef struct int16Store{ unsigned char theBytes[3]; int16_t theInt; } int16Store; //contains the 16bit- int and byte representation of the int
+typedef struct int32Store{ unsigned char theBytes[5]; int32_t theInt; } int32Store;;//contains the 32bit- int and byte representation of the int
 
 
 
-struct request
+struct request //Struct containing each part of a server request
 {
-  uint8_t TML;
-  uint8_t requestID;
-  uint8_t opCode;
-  uint8_t numOperands;
+  uint8_t TML;		//Total Message Length
+  uint8_t requestID;	//ID of the request (request number)
+  uint8_t opCode;	//Code for the operation to perform
+  uint8_t numOperands;	//Number of operands for operation
   int16Store operandOne;
   int16Store operandTwo;
 } __attribute__((__packed__));//minimizes space
@@ -45,12 +45,12 @@ struct request
 typedef struct request request_t; //struct for request to server
 
 
-struct response
+struct response //Struct containing each part of an incoming response from the server
 {
-  uint8_t TML;
-  uint8_t requestID;
-  uint8_t errorCode;
-  int32Store result;
+  uint8_t TML;		//Total Message Length
+  uint8_t requestID;	//ID of the request (request number)
+  uint8_t errorCode;	
+  int32Store result;	
 } __attribute__((__packed__));//minimizes space
 
 typedef struct response response_t; //struct for response from server
@@ -72,7 +72,7 @@ void *get_in_addr(struct sockaddr *sa)
 
 
 
-int32_t get32FromBytes(unsigned char * theBytes){
+int32_t get32FromBytes(unsigned char * theBytes){ //get int32 from byte array
    int32_t retInt = 0;
    retInt = (retInt & 0x00ffffff ) | (theBytes[0] << 24);
    retInt = (retInt & 0xff00ffff ) | (theBytes[1] << 16);
@@ -81,29 +81,15 @@ int32_t get32FromBytes(unsigned char * theBytes){
    return retInt;
 }
 
-int16_t get16FromBytes(unsigned char * theBytes){
-   int16_t retInt = 0;
-   retInt = (retInt & 0x00ff ) | (theBytes[0] << 8);
-   retInt = (retInt & 0xff00 ) | (theBytes[1]);
-   return retInt;
-}
 
-void getBytesFrom32(unsigned char * theBytes, int32_t unpackInt){
-   theBytes[0] = (unpackInt >> 24 );
-   theBytes[1] = (unpackInt >> 16 ) & 0x000000ff;
-   theBytes[2] = (unpackInt >> 8 )  & 0x000000ff;
-   theBytes[3] = (unpackInt)        & 0x000000ff;
-   theBytes[4] = '\0';
-}
-
-void getBytesFrom16(unsigned char * theBytes, int16_t unpackInt){
+void getBytesFrom16(unsigned char * theBytes, int16_t unpackInt){ // get byte array from a int16
    theBytes[0] = (unpackInt >> 8 )  & 0x00ff;
    theBytes[1] = (unpackInt)        & 0x00ff;
    theBytes[2] = '\0';
 }
 
 
-void getTotalMessage(unsigned char * theBytes, request_t targetRequest){
+void getTotalMessage(unsigned char * theBytes, request_t targetRequest){ //Converts request_t struct into array of bytes to send
     theBytes[0] = targetRequest.TML;
     theBytes[1] = targetRequest.requestID;
     theBytes[2] = targetRequest.opCode;
@@ -114,7 +100,7 @@ void getTotalMessage(unsigned char * theBytes, request_t targetRequest){
     theBytes[7] = targetRequest.operandTwo.theBytes[1];
 }
 
-response_t remakeResponse(unsigned char * theBytes){
+response_t remakeResponse(unsigned char * theBytes){ //Fills in response_t struct with data received from server
     response_t returnResponse;
     returnResponse.TML = theBytes[0];
     returnResponse.requestID = theBytes[1];
@@ -131,7 +117,7 @@ response_t remakeResponse(unsigned char * theBytes){
     return returnResponse;
 }
 
-request_t getRequest(uint8_t reqNum) {
+request_t getRequest(uint8_t reqNum) { //Prompts user for request info
    uint16_t op1;
    uint16_t op2;
    uint8_t opc;
@@ -175,7 +161,7 @@ request_t getRequest(uint8_t reqNum) {
 }
 
 
-long timediff(clock_t t1, clock_t t2) {
+long timediff(clock_t t1, clock_t t2) { //calculates elapsed time in milliseconds of clocks in main()
     long elapsed;
     elapsed = ((double)t2 - t1) / CLOCKS_PER_SEC * 1000;
     return elapsed;
@@ -185,21 +171,22 @@ long timediff(clock_t t1, clock_t t2) {
 
 int main(int argc, char *argv[])
 {
-	int sockfd, numbytes;  
-	unsigned char buf[MAXDATASIZE];
-   unsigned char sendBytes[MAXSENDLEN];
-   unsigned char recBytes[MAXRECLEN];
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	char s[INET6_ADDRSTRLEN];
-   int portNum;
-   request_t req;
-   // Variables for clocks and calculating elapsed time.
-   clock_t t1, t2;
-   int i;
-   float x = 2.7182;
-   long elapsed;
-
+  int sockfd, numbytes;  
+  unsigned char buf[MAXDATASIZE];
+  unsigned char sendBytes[MAXSENDLEN];
+  unsigned char recBytes[MAXRECLEN];
+  struct addrinfo hints, *servinfo, *p;
+  int rv;
+  char s[INET6_ADDRSTRLEN];
+  int portNum;
+  request_t req;
+  
+  ////////////////////////////////////////////////
+  clock_t t1, t2;  ///////////////////////////////
+  int i;	   ///////////// Clock ///////////
+  float x = 2.7182;/////////// Variables /////////
+  long elapsed;	   ///////////////////////////////
+  ////////////////////////////////////////////////
  
 	if (argc != 3) {
 	    fprintf(stderr,"usage: syntax: client <Host Name> <Port Number>\n");
@@ -207,37 +194,37 @@ int main(int argc, char *argv[])
 	}
    
    
-	memset(&hints, 0, sizeof hints); //make sure hints is empty
-	hints.ai_family = AF_UNSPEC; //IPv4 or IPv6
-	hints.ai_socktype = SOCK_STREAM; //TCP socket
+	memset(&hints, 0, sizeof hints);   //make sure hints is empty
+	hints.ai_family = AF_UNSPEC; 	   //IPv4 or IPv6
+	hints.ai_socktype = SOCK_STREAM;   //TCP socket
 
-	if ((rv = getaddrinfo(argv[1], argv[2], &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
+	if ((rv = getaddrinfo(argv[1], argv[2], &hints, &servinfo)) != 0) { //fills hints with address info
+	  fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));	    
+	  return 1;						    	    
 	}
 
 	// loop through all the results and connect to the first we can
 	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("client: socket");
-			continue;
-		}
+	  if ((sockfd = socket(p->ai_family, p->ai_socktype,
+			       p->ai_protocol)) == -1) {
+	    perror("client: socket");
+	    continue;
+	  }
       
-		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			perror("client: connect");
-        close(sockfd);
-			continue;
-		}
+	  if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) { //connect to server
+	    perror("client: connect");
+	    close(sockfd);
+	    continue;
+	  }
       
       
-		break;
+	  break;
 	}
    
 
 	if (p == NULL) {
-		fprintf(stderr, "client: failed to connect\n");
-		return 2;
+	  fprintf(stderr, "client: failed to connect\n");
+	  return 2;
 	}
 
 
@@ -249,59 +236,71 @@ int main(int argc, char *argv[])
    
    int iNum = 0;
    int cont = 1;
-   while(cont == 1) {
+   while(cont == 1) { // Loops until user selects not to after a request
       uint8_t reqNum = iNum;
       req = getRequest(reqNum);
       
-      getTotalMessage(sendBytes, req);
+      getTotalMessage(sendBytes, req); //Builds byte array to send to server
       
       
       printf("The Bytes Client intends to send are: ");
       int j;
       for(j = 0; j < 8; j++){
-         printf(" %02x", sendBytes[j] & 0xff);
+	printf(" %02x", sendBytes[j] & 0xff); 	//Print out bytes client is going to send
       }
       printf("\n");
 
-      send(sockfd,sendBytes,8,0);
-      //Start clock
-      t1 = clock();
-      for (i=0; i < 1000000; i++) {
-         x = x * 3.1415;
-      }
-   
-   	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-   	    perror("client: recv");
-   	    exit(1);
-   	}
-      t2 = clock();
-      elapsed = timediff(t1, t2);
-   
-   	int messageLength = buf[0];
-      int totBytes;
-      for(totBytes = 0; totBytes < messageLength; totBytes++){
-            recBytes[totBytes] = buf[totBytes];
-      }
-      int k;
-      printf("\nThe Bytes Client received are: ");
-      for(k = 0; k < 8; k++){
-          printf(" %02x", recBytes[k] & 0xff);
-      }
-      printf("\n");
-        
-      response_t currentResponse = remakeResponse(recBytes);
-      printf("The server replied with the following requestID: %d\n", currentResponse.requestID);
-      printf("The server replied with the following result: %d\n", currentResponse.result.theInt);
-      t2 = clock();
-      printf("elapsed: %ld ms\n", elapsed);
+      send(sockfd,sendBytes,8,0); //Sends the request to the server
+
+
+/////////////////////////////////////////////////////////////////////
+      t1 = clock();		       //////////////////////////////	
+      for (i=0; i < 1000000; i++) {    //////Start Clock Timer///////
+	x = x * 3.1415;		       //////////////////////////////
+      }				       //////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
       
-      printf("Would you like to continue? (1 for yes, 0 for no): ");
-      scanf("%d" SCNd8, &cont);
-        
-      iNum = iNum + 1;
-   }
-	close(sockfd);
+      
+      if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) { //receive data from server
+	perror("client: recv");
+	exit(1);
+      }
+
+///////////////////////////////////////////////////////////////////////
+	t2 = clock();		      ///////////////Stop//////////////
+	elapsed = timediff(t1, t2);   ////////////Clock Timer//////////
+///////////////////////////////////////////////////////////////////////
    
-	return 0;
+	int messageLength = buf[0]; //gets the length of received message from first byte
+	
+	int totBytes;
+	for(totBytes = 0; totBytes < messageLength; totBytes++){ // Copy the received bytes to recBytes[]
+	  recBytes[totBytes] = buf[totBytes];
+	}
+
+	
+	int k;
+	printf("\nThe Bytes Client received are: ");
+	for(k = 0; k < 7; k++){
+          printf(" %02x", recBytes[k] & 0xff); // Outputs the bytes received in hex form
+	}
+	printf("\n");
+	
+        
+	response_t currentResponse = remakeResponse(recBytes); //fills a response_t struct with data that was received
+	printf("The server replied with the following requestID: %d\n", currentResponse.requestID); //get requestID from response_t 
+	printf("The server replied with the following result: %d\n", currentResponse.result.theInt); //get result from response_t currentResponse
+	
+	printf("Time elapsed: %ld ms\n", elapsed);
+      
+	printf("Would you like to continue? (1 for yes, 0 for no): ");
+	scanf("%d" SCNd8, &cont);
+        
+	iNum = iNum + 1; //Increments the request number to give next request correct requestID
+   }
+   close(sockfd);
+   
+   return 0;
 }
 
